07999511c0ed05042ca29328f9c30888f2caf867
apalm3 <apalmer21379@gmail.com>
2023-04-13 16:17:41 -0400

    login

--- app.py
+++ app.py
@@ -1,49 +1,94 @@
-from flask import Flask, render_template, request, redirect, url_for, session
-import sqlite3
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from models import User, List, Lawyer
+from database import db
 
+# create the app
 app = Flask(__name__)
-app.secret_key = 'key'
 
-@app.route('/')
+# configure the SQLite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///listigator.sqlite3"
+
+# initialize the app with the extension
+db.init_app(app)
+
+@app.route('/', methods=['GET'])
+def index():
+    """Displays search form and results table"""
+    return redirect('login')
+
+@app.route('/register',methods = ['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        if (request.form['password'] == request.form['confirm_password']):
+            user = User(username, request.form['password'])
+            db.session.add(user)
+            db.session.commit()
+            return redirect(url_for('login'))
+        
+        return redirect(url_for('login'))
+    else:
+        return render_template('register.html')
+
+@app.route('/login',methods = ['POST', 'GET'])
+def login():
+    message = None
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        
+        if username and password:
+            user = User.query.filter_by(username=username).first()
+            if user and check_password_hash(user.password_hash, request.form['password']):
+                return redirect(url_for('home'))
+            else:
+                message = 'Invalid login information.'
+        else:
+            message = 'Missing login information.'
+            
+    return render_template('login.html', message=message)
+
+@app.route('/home',methods = ['POST', 'GET'])
 def home():
     return render_template('home.html')
 
-@app.route('/results', methods=['POST'])
-def results():
-    field = request.form['field']
-    name = request.form['name']
-    city = request.form['city']
-    probono = request.form.get('probono')
-    phone = request.form['phone']
+# @app.route('/results', methods=['POST'])
+# def results():
+#     field = request.form['field']
+#     name = request.form['name']
+#     city = request.form['city']
+#     probono = request.form.get('probono')
+#     phone = request.form['phone']
 
-    query = "SELECT * FROM Lawyer INNER JOIN Field ON Field.LicenseNumber=Lawyer.LicenseNumber"
-    parameters = {}
-    conditions = []
+#     query = "SELECT * FROM Lawyer INNER JOIN Field ON Field.LicenseNumber=Lawyer.LicenseNumber"
+#     parameters = {}
+#     conditions = []
 
-    if field:
-        conditions.append("Field LIKE :field")
-        parameters['field'] = f'%{field}%'
-    if name:
-        conditions.append("Name LIKE :name")
-        parameters['name'] = f'%{name}%'
-    if city:
-        conditions.append("City LIKE :city")
-        parameters['city'] = f'%{city}%'
-    if probono is not None:
-        conditions.append("Status = 'Pro Bono'")
-        parameters['probono'] = probono
-    if phone:
-        conditions.append("Phone LIKE :phone")
-        parameters['phone'] = f'%{phone}%'
+#     if field:
+#         conditions.append("Field LIKE :field")
+#         parameters['field'] = f'%{field}%'
+#     if name:
+#         conditions.append("Name LIKE :name")
+#         parameters['name'] = f'%{name}%'
+#     if city:
+#         conditions.append("City LIKE :city")
+#         parameters['city'] = f'%{city}%'
+#     if probono is not None:
+#         conditions.append("Status = 'Pro Bono'")
+#         parameters['probono'] = probono
+#     if phone:
+#         conditions.append("Phone LIKE :phone")
+#         parameters['phone'] = f'%{phone}%'
     
-    if conditions:
-        query += " WHERE " + " AND ".join(conditions)
+#     if conditions:
+#         query += " WHERE " + " AND ".join(conditions)
 
-    with sqlite3.connect('WALawyers.sqlite') as conn:
-        cursor = conn.cursor()
-        results = cursor.execute(query, parameters).fetchall()
+#     with sqlite3.connect('WALawyers.sqlite') as conn:
+#         cursor = conn.cursor()
+#         results = cursor.execute(query, parameters).fetchall()
 
-    return render_template('results.html', results=results)
+#     return render_template('results.html', results=results)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,86 @@
+from sqlalchemy.orm import relationship
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from database import db
+
+class FieldEnum(db.Enum):
+    juvenile = 1
+    guardianships = 2
+    cannabis = 3
+    civil_rights = 4
+    criminal = 5
+    human_rights = 6
+    immigration_and_naturalization = 7
+    native_american_law = 8
+    lgbtq = 9
+
+# need to create a session to interact with database
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    username = db.Column(db.String(50))
+    password_hash = db.Column(db.String(128))
+
+    lists = db.relationship('List', backref='user', lazy=True)
+    #below our user model, we will create our hashing functions
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
+    def __init__(self, username, password):
+        self.username = username
+        self.set_password(password)
+    
+    def __repr__(self):
+        return "<User(username='%s')>" % (
+        self.username,
+    )
+
+# helper table for many to many relationship
+
+list_lawyers = db.Table('list_lawyers',
+    db.Column('lawyer_id', db.Integer, db.ForeignKey('lawyer.id'), primary_key=True),
+    db.Column('list_id', db.Integer, db.ForeignKey('list.id'), primary_key=True)
+)
+
+# create a one to many relationship between users and lists 
+# (each user has a collection of lists)
+
+class List(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'),
+        nullable=False)
+    
+    title = db.Column(db.String(50), nullable=False)
+    lawyers = db.relationship('Lawyer', secondary=list_lawyers, lazy='subquery',
+        backref=db.backref('lists', lazy=True))
+    
+    def __repr__(self):
+        return "<List(title='%s')>" % self.title
+
+# create a many to many relationship between lists and lawyers
+# (each list has a collection of lawyers)
+
+class Lawyer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    
+    license_number =db.Column(db.Integer, nullable=False)
+    name = db.Column(db.String(50), nullable=False)
+    
+    city = db.Column(db.String(50), nullable=False)
+    status = db.Column(db.String(20), nullable=False)
+    phone = db.Column(db.String(20), nullable=False)
+    
+    field = db.Column(db.String(20), nullable=False)
+    
+    def __repr__(self):
+        return "<Lawyer(name='%s', city='%s', status='%s', phone='%s')>" % (
+        self.name,
+        self.city,
+        self.status,
+        self.phone,
+    )
